e34f79042d71c7e811d7ebb43308683ee92fe5a4
Olaf Ronald <152807076+olafronald@users.noreply.github.com>
2023-12-04 00:14:52 +0200

    Create iOS Game.dart

---
+++ iOS Game.dart
@@ -0,0 +1,26 @@
+import 'dart:io';
+
+import 'package:audio_video_progress_bar/audio_video_progress_bar.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+
+import 'package:flutter_svg/svg.dart';
+import 'package:get_it/get_it.dart';
+import 'package:provider/provider.dart';
+
+  const CidOrIpfs(
+      {Key? key,
+      required this.viewCid,
+      required this.viewIpfs,
+      required this.type})
+      : super(key: key);
+
+class Loading {
+  Loading();
+
+  void dismiss() {
+    navigatorKey.currentState?.pop();
+  }
+
+    return showDialog(
+      context: navigatorKey.currentState!.overlay!.context,
+      barrierDismissible: true,)
